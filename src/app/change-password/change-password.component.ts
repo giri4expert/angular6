685b962c05b010253bea399b332b0742ecb025cf
Gireesh Babu <gireesh.babu@expertzlab.com>
2019-08-27 15:32:47 +0530

    reactive froms and templates form validations completed

--- src/app/change-password/change-password.component.ts
+++ src/app/change-password/change-password.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-change-password',
@@ -9,13 +10,32 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class ChangePasswordComponent implements OnInit {
 
   login:FormGroup
-
   constructor(private fb:FormBuilder) { }
 
   ngOnInit() {
     this.login = this.fb.group({
-      login:['', [Validators.required, Validators.minLength(6)]],
+      lmethod:['login'],
+      login:['', ],
+      email:['', []],
       password:['',this.passwordValidation]
+    })
+
+    this.login.valueChanges.subscribe((val) => {
+      console.log('event detected:'+val.lmethod)
+      if(val.lmethod === 'email' || val.lmethod === 'login'){
+        if(val.lmethod === 'email'){
+          const emctrl = this.login.get('email')
+          emctrl.setValidators([Validators.required,Validators.email])
+          const lgnCtrl = this.login.get('login')
+          lgnCtrl.setValidators([]);
+        } else {
+          const emctrl = this.login.get('email')
+          emctrl.setValidators([])
+          const lgnCtrl = this.login.get('login')
+          lgnCtrl.setValidators([Validators.required]);
+        }
+      
+    } 
     })
   }
 
@@ -26,5 +46,10 @@ export class ChangePasswordComponent implements OnInit {
     }
     console.log('password length NOT satisfied')
   }
+
+  setLgnMethodCtrl(value){
+    this.login.updateValueAndValidity()
+  }
+
 
 }
